4fea5f65540251e5feb960986e750886307b75d8
tac-yacht <tac-yacht@users.noreply.github.com>
2021-07-24 23:12:32 +0900

    parameter parse and expand

--- src/main/java/com/iyher/gradle/plugin/codeartifact_repository/CodeArtifactResolver.java
+++ src/main/java/com/iyher/gradle/plugin/codeartifact_repository/CodeArtifactResolver.java
@@ -1,4 +1,12 @@
 package com.iyher.gradle.plugin.codeartifact_repository;
+
+import static org.apache.commons.lang3.ObjectUtils.*;
+
+import java.net.URI;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.slf4j.Logger;
@@ -31,12 +39,101 @@ public class CodeArtifactResolver {
 			}
 		};
 	}
-	
+
 	CodeArtifactRepository parseResource(Closure<?> closure) {
 		log.debug("start: convert closure to codeartifact configuration");
-		CodeArtifactRepository base = (CodeArtifactRepository) project.configure(new CompositRepositoryExclusiveToCodeartifact(), closure);
+		CodeArtifactRepository origin = (CodeArtifactRepository) project.configure(new CompositRepositoryExclusiveToCodeartifact(), closure);
 		log.debug("end  : convert closure to codeartifact configuration");
+
+		return parseResource(origin);
+	}
+
+	CodeArtifactRepository parseResource(CodeArtifactRepository origin) {
+		if(!isValid(origin)) {
+			throw new IllegalStateException("url or arn or (region and domain and repository) required");
+		}
 		
-		return base;
+		if (allNotNull(origin.getDomainName(), origin.getDomainOwner(), origin.getRegion(),
+			origin.getRepository(),origin.getUrl()
+		)) {
+			log.debug("has all params");
+			return origin;
+		}
+
+		Optional<CodeArtifactRepository> urlBase = Optional.of(origin)
+			.map(CodeArtifactRepository::getUrl)
+			.map(CodeArtifactURLParser::parse)
+		;
+
+		Optional<CodeArtifactRepository> arnBase = Optional.of(origin)
+			.map(CodeArtifactRepository::getArn)
+			.map(CodeArtifactARNParser::parse)
+			.map(element -> {
+				URI url = CodeArtifactURLFinder.find(element);
+				element.setUrl(url);
+				return element;
+			})
+		;
+
+		Optional<CodeArtifactRepository> paramBase = Optional.of(origin)
+			.map(CodeArtifactURLFinder::find)
+			.map(url -> {
+				CodeArtifactRepository result = new CompositRepositoryExclusiveToCodeartifact();
+				result.setUrl(url);
+				return result;
+			})
+		;
+		
+
+		CodeArtifactRepository result = new CompositRepositoryExclusiveToCodeartifact();
+		mapAndFindFirst(CodeArtifactRepository::getUrl, Optional.of(origin), urlBase, arnBase, paramBase)
+			.ifPresent(result::setUrl);
+
+		mapAndFindFirst(CodeArtifactRepository::getDomainName, Optional.of(origin), urlBase, arnBase, paramBase)
+			.ifPresent(result::setDomainName);
+		mapAndFindFirst(CodeArtifactRepository::getDomainOwner, Optional.of(origin), urlBase, arnBase, paramBase)
+			.ifPresent(result::setDomainOwner);
+		mapAndFindFirst(CodeArtifactRepository::getRegion, Optional.of(origin), urlBase, arnBase, paramBase)
+			.ifPresent(result::setRegion);
+		mapAndFindFirst(CodeArtifactRepository::getRepository, Optional.of(origin), urlBase, arnBase, paramBase)
+			.ifPresent(result::setRepository);
+
+		mapAndFindFirst(CodeArtifactRepository::getArn, Optional.of(origin), urlBase, arnBase, paramBase)
+			.ifPresent(result::setArn);
+	
+		
+		return result;
+	}
+	
+	boolean isValid(CodeArtifactRepository repo) {
+		if (allNotNull(repo.getUrl())) {
+			return true;
+		}
+
+		if (allNotNull(repo.getArn())) {
+			return true;
+		}
+		
+		if (allNotNull(repo.getDomainName(), repo.getRepository())) {
+			if (allNotNull(repo.getRegion(), repo.getDomainOwner())) {
+				return true;
+			} else {
+				//TODO 動的解決できるところなのであとで対応
+				log.warn("now unsupported. dynamic resolve is future operation.");
+				return false;
+			}
+		}
+		
+		return false;
+	}
+	
+	@SafeVarargs
+	static final <T,R> Optional<R> mapAndFindFirst(Function<T,R> map, Optional<T> ... opts) {
+		Function<Optional<T>, Optional<R>> wappedFunction = t -> t.map(map);
+		return Stream.of(opts)
+			.map(wappedFunction)
+			.filter(Optional::isPresent)
+			.map(Optional::get)
+		.findFirst();
 	}
 }
